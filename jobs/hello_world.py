22509c3bb43b132963c458d8697a102d8540b685
Roger <roger@rogerperkin.co.uk>
2025-08-13 15:01:46 +0000

    more logging in hello world

--- jobs/hello_world.py
+++ jobs/hello_world.py
@@ -11,6 +11,20 @@ class HelloWorldJob(Job):
         self.logger.info("Hello, Nautobot!")
         return "Hello, Nautobot! Job ran successfully."
     
+class HelloJobsWithLogs(Job):
+
+    class Meta:
+        name = "Hello Jobs with Logs"
+        description = "Hello Jobs with different log types"
+
+    def run(self):
+        self.logger.info("This is an info type log.")
+        self.logger.debug("This is a debug type log.")
+        self.logger.warning("This is a warning type log.")
+        self.logger.error("This is an error type log.")
+        self.logger.critical("This is a critical type log.")
+    
 register_jobs(
     HelloWorldJob,
+    HelloJobsWithLogs
 )
