7bc29fcaadb6ad794c4a0516c8b04ed0f2241cda
Roger <roger@rogerperkin.co.uk>
2025-08-13 11:51:49 +0000

    fix

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,7 +1,7 @@
 from nautobot.core.celery import register_jobs
-from .shutdown_interface import JunosDisableInterface
+#from .shutdown_interface import JunosDisableInterface
 from .hello_world import HelloWorldJob
 
-register_jobs(JunosDisableInterface, HelloWorldJob)
+register_jobs(HelloWorldJob)
 
 
