028576b2b13a31f4856fdcf5cbcea01e6998fc32
Roger <roger@rogerperkin.co.uk>
2025-08-13 15:59:11 +0000

    added management ip to verify

--- jobs/data_quality.py
+++ jobs/data_quality.py
@@ -11,6 +11,42 @@ logger = get_task_logger(__name__)
 
 name = "Data Quality"
 
+class VerifyManagementIP(Job):
+    class Meta:
+        name = "Verify Management IP"
+        description = "Verify that a device has a management IP assigned"
+
+    location = MultiObjectVar(
+        model=Location,
+        required=False,
+        description="Filter by Location"
+    )
+    device_role = MultiObjectVar(
+        model=Role,
+        required=False,
+        description="Filter by Device Role"
+    )
+    device_type = MultiObjectVar(
+        model=DeviceType,
+        required=False,
+        description="Filter by Device Type"
+    )
+
+    def run(self, location, device_role, device_type):
+        devices = Device.objects.all()
+        if location:
+            devices = devices.filter(location__in=location)
+        if device_role:
+            devices = devices.filter(role__in=device_role)
+        if device_type:
+            devices = devices.filter(device_type__in=device_type)
+
+        for device in devices:
+            if device.primary_ip:
+                logger.info("Management IP is defined: %s", device.primary_ip, extra={"obj": device})
+            else:
+                logger.warning("Management IP is NOT defined", extra={"obj": device})
+
 
 def normalize(query_set):
     """Returns a list of names or other identifiers for logging.
@@ -209,4 +245,5 @@ register_jobs(
     VerifyPrimaryIP,
     VerifyHasRack,
     VerifyCircuitTermination,
+    VierfyManagementIP
 )
